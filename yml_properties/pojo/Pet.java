9fd2213a97e88d9256009496c6d3b25d5bbb6549
wyl <82953346+pepsi-wyl@users.noreply.github.com>
2021-11-19 19:48:56 +0800

    Update Pet.java

--- yml_properties/pojo/Pet.java
+++ yml_properties/pojo/Pet.java
@@ -12,8 +12,10 @@ import org.springframework.stereotype.Component;
  * @date 2021/11/14.20点58分
  */
 
-@ConfigurationProperties(prefix = "pet")
 @Component("pet")                               //注册组件
+//@EnableConfigurationProperties(Pet.class)     //开启car配置绑定功能  组件没有注册时 在config类中进行配置该注解
+
+@ConfigurationProperties(prefix = "pet")        //读取配置文件中的数据 前缀为pet
 
 @Data
 @AllArgsConstructor
